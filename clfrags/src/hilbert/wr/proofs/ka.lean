9a5ec9e00e4e788a14e634942cce9382365dcf72
Vitor Greati <greati@ufrn.edu.br>
2019-08-16 01:45:42 -0300

    Start proofs of ka

--- clfrags/src/hilbert/wr/proofs/ka.lean
+++ clfrags/src/hilbert/wr/proofs/ka.lean
@@ -6,13 +6,27 @@ namespace clfrags
             namespace ka
 
                 theorem ka₀ {a b c : Prop} (h₁ : ka a b c) : a :=
-                sorry
+                    have h₃ : ka (ka a b c) (ka a b c) a, from ka₁ h₁ h₁,
+                    have h₄ : ka (ka a b c) a (ka a b c), from ka₃ h₃,
+                    have h₅ : ka (ka a b c) a a, from ka₆ h₄,
+                    show a, from ka₂ h₅
+
+                theorem ka₄' {a b c d : Prop} (h₁ : ka d (ka d c a) b) : ka d c (ka d a b) :=
+                    have h₂ : ka d b (ka d c a), from ka₃ h₁,
+                    have h₃ : ka d (ka d b c) a, from ka₄ h₂,
+                    have h₄ : ka d a (ka d b c), from ka₃ h₃,
+                    have h₅ : ka d (ka d a b) c, from ka₄ h₄,
+                    show ka d c (ka d a b), from ka₃ h₅
 
                 theorem ka₁_ast {a b c d e : Prop} (h₁ : ka e d a) (h₂ : ka e d b) : ka e d (ka a b c) :=
-                sorry
+                    have h₃ : e, from ka₀ h₂,
+                    have h₄ : ka e (ka e d b) c, from ka₁ h₃ h₂,
+                    have h₅ : ka e d (ka e b c), from ka₄' h₄,
+                    show ka e d (ka a b c), from ka₅ h₁ h₅
 
                 theorem ka₂_ast {a b c d : Prop} (h₁ : ka d c (ka a b b)) : ka d c b :=
-                sorry
+                    have h₂ : ka d c (ka d b b), from ka₇ h₁,
+                    sorry
 
                 theorem ka₃_ast {a b c d e : Prop} (h₁ : ka e d (ka a b c)) : ka e d (ka a c b) := 
                 sorry
