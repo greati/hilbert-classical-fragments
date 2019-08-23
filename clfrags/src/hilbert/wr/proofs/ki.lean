680086b1cdb46c842be7aa5280df35884b4a1528
Vitor Greati <greati@ufrn.edu.br>
2019-08-22 23:50:56 -0300

    Add additional rules of ki

--- clfrags/src/hilbert/wr/proofs/ki.lean
+++ clfrags/src/hilbert/wr/proofs/ki.lean
@@ -5,7 +5,37 @@ namespace clfrags
         namespace wr
             namespace ki
 
+                theorem ka₁₀ {a b c : Prop} (h₁ : a) (h₂ : c) : ki a b c :=
+                    sorry
+
+                theorem ka'₃ {a b c d : Prop} (h₁ : a) :  
+                    ki a (ki a b (ki a c d)) (ki a (ki a b c) (ki a b d)) :=
+                    sorry
+
+                theorem ka'₄ {a b c d : Prop} (h₁ : a) :  
+                    ki a (ki a c b) c :=
+                    sorry
+
                 theorem ka₀ {a b c : Prop} (h₁ : ki a b c) : a :=
+                    sorry
+            
+                theorem ka₁₁ {a b : Prop} (h₁ : a) : ki a b b :=
+                    sorry
+
+                theorem ka₁₂ {a b c d : Prop} (h₁ : ki a b (ki a c d)) : ki a c (ki a b d) :=
+                    sorry
+
+                theorem ka₁₃ {a b c : Prop} (h₁ : ki a b (ki a b c)) : ki a b c :=
+                    sorry
+
+                theorem ka₁₄ {a b c d : Prop} (h₁ : ki a b d) : ki b b (ki a c d) :=
+                    sorry
+
+                theorem ka₁₅ {a b c d : Prop} (h₁ : ki a b d) : ki c c (ki a b d) :=
+                    sorry
+
+                theorem ka₁₆ {a b c d : Prop} (h₁ : ki a b c) (h₂ : ki a c d) : 
+                    ki a b d :=
                     sorry
 
             end ki
