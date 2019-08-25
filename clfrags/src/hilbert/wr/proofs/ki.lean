6b7a70a75494051b3da15ce8a411f034a5a78504
Vitor Greati <greati@ufrn.edu.br>
2019-08-25 12:37:36 -0300

    Add more proofs for ki

--- clfrags/src/hilbert/wr/proofs/ki.lean
+++ clfrags/src/hilbert/wr/proofs/ki.lean
@@ -29,13 +29,21 @@ namespace clfrags
                         show a, from ki₁ h₂ h₄
             
                 theorem ki₁₁ {a b : Prop} (h₁ : a) : ki a b b :=
-                    sorry
+                    have h₂ : ki a (ki a b (ki a a b)) (ki a (ki a b a) (ki a b b)), from ki'₃ h₁,
+                    have h₃ : ki a b (ki a a b), from ki₂ h₁,
+                    have h₄ : ki a (ki a b a) (ki a b b), from ki₁ h₃ h₂,
+                    have h₅ : ki a b a, from ki₁₀ h₁ h₁,
+                    show ki a b b, from ki₁ h₅ h₄
 
                 theorem ki₁₂ {a b c d : Prop} (h₁ : ki a b (ki a c d)) : ki a c (ki a b d) :=
                     sorry
 
                 theorem ki₁₃ {a b c : Prop} (h₁ : ki a b (ki a b c)) : ki a b c :=
-                    sorry
+                    have h₂ : a, from ki₀ h₁,
+                    have h₃ : ki a (ki a b (ki a b c)) (ki a (ki a b b) (ki a b c)), from ki'₃ h₂,
+                    have h₄ : ki a (ki a b b) (ki a b c), from ki₁ h₁ h₃,
+                    have h₅ : ki a b b, from ki₁₁ h₂,
+                    show ki a b c, from ki₁ h₅ h₄
 
                 theorem ki₁₄ {a b c d : Prop} (h₁ : ki a b d) : ki b b (ki a c d) :=
                     sorry
@@ -45,7 +53,11 @@ namespace clfrags
 
                 theorem ki₁₆ {a b c d : Prop} (h₁ : ki a b c) (h₂ : ki a c d) : 
                     ki a b d :=
-                    sorry
+                    have h₃ : a, from ki₀ h₁,
+                    have h₄ : ki a (ki a b (ki a c d)) (ki a (ki a b c) (ki a b d)), from ki'₃ h₃,
+                    have h₅ : ki a b (ki a c d), from ki₁₀ h₃ h₂,
+                    have h₆ : ki a (ki a b c) (ki a b d), from ki₁ h₅ h₄,
+                    show ki a b d, from ki₁ h₁ h₆
 
                 theorem ki₁_ast {a b c d e : Prop} (h₁ : ki d e b) (h₂ : ki d e (ki a b c)) : ki d e c :=
                     sorry
