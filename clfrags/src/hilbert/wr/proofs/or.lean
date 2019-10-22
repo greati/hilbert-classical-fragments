7dee128a9d0a619f6dd639d2e42d25c5d93d0348
Vitor Greati <greati@ufrn.edu.br>
2019-10-22 17:49:09 -0300

    change letters for or frag

--- clfrags/src/hilbert/wr/proofs/or.lean
+++ clfrags/src/hilbert/wr/proofs/or.lean
@@ -16,29 +16,29 @@ namespace clfrags
                     have h₅ : or (or b c) a, from or.d₄ h₄,
                     show or a (or b c), from or.d₃ h₅
         
-                theorem d₁_or {a b d : Prop} (h₁ : or d a) : or d (or a b) :=
-                    have h₂ : or (or d a) b, from or.d₁ h₁,
-                    show or d (or a b), from or.d₄' h₂
+                theorem d₁_or {a b c : Prop} (h₁ : or c a) : or c (or a b) :=
+                    have h₂ : or (or c a) b, from or.d₁ h₁,
+                    show or c (or a b), from or.d₄' h₂
 
-                theorem d₂_or {d a : Prop} (h₁ : or d (or a a)) : or d a :=
-                    have h₂ : or (or d a) a, from or.d₄ h₁,
-                    have h₃ : or a (or d a), from or.d₃ h₂,
-                    have h₄ : or d (or a (or d a)), from or.d₁' h₃,
-                    have h₅ : or (or d a) (or d a), from or.d₄ h₄,
-                    show or d a, from or.d₂ h₅
+                theorem d₂_or {a b : Prop} (h₁ : or b (or a a)) : or b a :=
+                    have h₂ : or (or b a) a, from or.d₄ h₁,
+                    have h₃ : or a (or b a), from or.d₃ h₂,
+                    have h₄ : or b (or a (or b a)), from or.d₁' h₃,
+                    have h₅ : or (or b a) (or b a), from or.d₄ h₄,
+                    show or b a, from or.d₂ h₅
 
-                theorem d₃_or {a b d : Prop} (h₁ : or d (or a b)) : or d (or b a) :=
-                    have h₂ : or (or d a) b, from or.d₄ h₁,
-                    have h₃ : or (or (or d a) b) a, from or.d₁ h₂,
-                    have h₄ : or (or d a) (or b a), from or.d₄' h₃,
-                    have h₅ : or d (or a (or b a)), from or.d₄' h₄,
-                    have h₆ : or (or a (or b a)) d, from or.d₃ h₅,
-                    have h₇ : or a (or (or b a) d), from or.d₄' h₆,
-                    have h₈ : or b (or a (or (or b a) d)), from or.d₁' h₇,
-                    have h₉ : or (or b a) (or (or b a) d), from or.d₄ h₈,
-                    have h₁₀ : or (or (or b a) (or b a)) d, from or.d₄ h₉,
-                    have h₁₁ : or d (or (or b a) (or b a)), from or.d₃ h₁₀,
-                    show or d (or b a), from or.d₂_or h₁₁
+                theorem d₃_or {a b c : Prop} (h₁ : or c (or a b)) : or c (or b a) :=
+                    have h₂ : or (or c a) b, from or.d₄ h₁,
+                    have h₃ : or (or (or c a) b) a, from or.d₁ h₂,
+                    have h₄ : or (or c a) (or b a), from or.d₄' h₃,
+                    have h₅ : or c (or a (or b a)), from or.d₄' h₄,
+                    have h₆ : or (or a (or b a)) c, from or.d₃ h₅,
+                    have h₇ : or a (or (or b a) c), from or.d₄' h₆,
+                    have h₈ : or b (or a (or (or b a) c)), from or.d₁' h₇,
+                    have h₉ : or (or b a) (or (or b a) c), from or.d₄ h₈,
+                    have h₁₀ : or (or (or b a) (or b a)) c, from or.d₄ h₉,
+                    have h₁₁ : or c (or (or b a) (or b a)), from or.d₃ h₁₀,
+                    show or c (or b a), from or.d₂_or h₁₁
 
                 theorem d₄_or {a b c d : Prop} (h₁ : or d (or a (or b c))) : or d (or (or a b) c) :=
                     have h₂ : or (or d a) (or b c), from or.d₄ h₁,
