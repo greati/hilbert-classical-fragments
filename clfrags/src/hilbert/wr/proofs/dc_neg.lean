3d22cf4238b24d18ce9d17c96269c52e52fd3cae
Vitor Greati <greati@ufrn.edu.br>
2019-10-30 15:55:07 -0300

    Rename rules of dc neg

--- clfrags/src/hilbert/wr/proofs/dc_neg.lean
+++ clfrags/src/hilbert/wr/proofs/dc_neg.lean
@@ -18,7 +18,7 @@ namespace clfrags
                     have h₃ : dc (dc e f c) (dc e f d) (dc b a (neg a)), from dcn₂ h₂,
                     show dc e f (dc c d (dc b a (neg a))), from dc.dc₇' h₃
 
-                theorem dcn₃ {a b : Prop} (h₁ : a) (h₂ : neg a) : b :=
+                theorem n₁ {a b : Prop} (h₁ : a) (h₂ : neg a) : b :=
                     have h₂ : dc a (neg a) b, from dc.dc₁ h₁ h₂,
                     have h₃ : dc (dc a (neg a) b) a b, from dc.dc₁ h₂ h₁,
                     have h₄ : dc a b (dc a (neg a) b), from dc.dc₅' (dc.dc₄' h₃),
@@ -26,7 +26,7 @@ namespace clfrags
                     have h₆ : dc a b b, from dcn₁ h₅,
                     show b, from dc.dc₂ h₆
             
-                theorem dcn₄ {a b : Prop} (h₁ : b) : dc b a (neg a) :=
+                theorem dcn₃ {a b : Prop} (h₁ : b) : dc b a (neg a) :=
                     have h₂ : dc a b b, from dc.dc₃ h₁,
                     have h₃ : dc a b (dc b a (neg a)), from dcn₂ h₂,
                     have h₄ : dc (dc a b b) (dc a b a) (neg a), from dc.dc₆' h₃,
