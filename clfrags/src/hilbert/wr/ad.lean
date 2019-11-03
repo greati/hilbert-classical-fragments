81e64e0dbddd92a6191bbf7cbee5b524e0311ca3
Vitor Greati <greati@ufrn.edu.br>
2019-11-03 03:08:57 -0300

    Start ad proofs

--- clfrags/src/hilbert/wr/ad.lean
+++ clfrags/src/hilbert/wr/ad.lean
@@ -12,7 +12,8 @@ namespace clfrags
                 axiom ad₆ : Π {a b c : Prop}, a → ad a b c
                 axiom ad₇ : Π {a b c d : Prop}, ad (ad c d c) a b → ad (ad c a b) (ad d a b) (ad c a b)
                 axiom ad₈ : Π {a b c d : Prop}, ad (ad c a b) (ad d a b) (ad c a b) → ad (ad c d c) a b 
-                axiom ad₉ : Π {a b c d e : Prop}, ad b d e → ad c a b → ad e d c
+                --axiom ad₉ : Π {a b c d e : Prop}, ad b d e → ad c a b → ad e d c
+                axiom ad₉ : Π {a b c d e : Prop}, ad a b c → ad d e a → ad d b c
                 axiom ad₁₀ : Π {a b c d e : Prop}, ad (ad e d c) a b → ad (ad e a b) (ad e d c) (ad e a b)
                 axiom ad₁₁ : Π {a b c d e : Prop}, ad (ad e a b) (ad e d c) (ad e a b) → ad (ad e d c) a b 
                 axiom ad₁₂ : Π {a : Prop}, ad a a a → a
@@ -30,6 +31,16 @@ namespace clfrags
                 axiom ad₂₃ : Π {a b c d e f : Prop}, ad f (ad b d e) f → ad f (ad c a b) f → ad f (ad e d c) f
                 axiom ad₂₄ : Π {a b c d e f : Prop}, ad f (ad (ad e d c) a b) f → ad f (ad (ad e a b) (ad e d c) (ad e a b)) f
                 axiom ad₂₅ : Π {a b c d e f : Prop}, ad f (ad (ad e a b) (ad e d c) (ad e a b)) f → ad f (ad (ad e d c) a b) f
+
+                notation a `or` b := ad a b a
+
+                constant M₂ : ∀ {a b c : Prop}, (a → b) → (c → a → b)
+                constant R : ∀ {a : Prop}, a → a
+                constant T₁ : ∀ {a b c : Prop}, (a → b) → (b → c) → (a → c)
+                constant δ_or₁ : ∀ {a b c : Prop}, (a → c) → (b → c) → ((a or b) → c)
+                constant T₂ : ∀ {a b c d : Prop}, (d → a → b) → (d → b → c) → (d → a → c)
+                constant δ_or₂ : ∀ {a b c d : Prop}, (d → a → c) → (d → b → c) → (d → (a or b) → c)
+
             end ad
         end wr
     end hilbert
--- clfrags/src/hilbert/wr/proofs/ad.lean
+++ clfrags/src/hilbert/wr/proofs/ad.lean
@@ -4,16 +4,45 @@ namespace clfrags
     namespace hilbert
         namespace wr
             namespace ad
-
+                
                 theorem ad₂₆ {a b c : Prop} (h₁ : a) (h₂ : b) : ad a b c :=
                     have h₃ : ad (ad a b c) b a, from ad₂ h₂,
                     show ad a b c, from ad₁ h₁ h₃
-
-                theorem ad'₄ {a b c : Prop} (h₁ : a) : ad c a (ad c a (ad b a c)) :=
+                
+                theorem ad₄' {a b c : Prop} (h₁ : a) : ad c a (ad c a (ad b a c)) :=
                     have h₂ : ad a a a, from ad₂₆ h₁ h₁,
                     have h₃ : ad (ad c a (ad c a (ad b a c))) a a, from ad₄ h₂,
                     show ad c a (ad c a (ad b a c)), from ad₁ h₁ h₃
 
+                theorem ad₂₇ {a b c d e : Prop} (h₁ : ad (ad a b c) d e) : ad (ad a d e) b c :=
+                    have h₂ : (ad a d e) or (ad a b c), from ad₁₀ h₁,
+                    have h₃ : (ad a b c) or (ad a d e), from ad₁₃ h₂,
+                    show ad (ad a d e) b c, from ad₁₁ h₃
+            
+                theorem ad₆' {a b : Prop} (h₁ : a) : a or b := ad₆ h₁
+
+                --theorem ad₂' {a b c d e : Prop} (h₁ : ad a d e) : 
+                --    ((ad c a c) or (ad c a b)) or ((ad a d e) or (ad c a b)) :=
+                --    sorry
+
+                theorem ad₈' {a b c d e f : Prop} (h₁ : ad (ad c a b) e f) : ad (ad (c or d) a b) e f := 
+                    have h₂ : (ad (ad c a b) e f) → ((ad c e f) or (ad c a b)), from ad₁₀,
+                    have h₃ : (ad c e f) → (ad (ad (c or d) a b) e f), from sorry,
+                    have h₄ : (ad c a b) → (ad (ad (c or d) a b) e f), from sorry,
+                    have h₅ : ((ad c e f) or (ad c a b)) → (ad (ad (c or d) a b) e f), from δ_or₁ h₃ h₄,
+                    show ad (ad (c or d) a b) e f, from (T₁ h₂ h₅) h₁
+                
+                theorem ad₁_ad {a b c d e : Prop} (h₁ : ad c d e) (h₂ : ad (ad a b c) d e) :
+                    ad a d e := 
+                    have h₃ : (ad (ad a b c) d e) → ((ad a d e) or (ad a b c)), from ad₁₀,
+                    have h₄ : ad a d e → ad a d e, from R,
+                    have h₅ : ad c d e → ad a b c → ad a d e, from ad₉,
+                    have h₆ : ad c d e → ad a d e → ad a d e, from M₂ h₄,
+                    have h₇ : ad c d e → ((ad a d e) or (ad a b c)) → ad a d e, from δ_or₂ h₆ h₅,
+                    have h₈ : ad c d e → (ad (ad a b c) d e) → ((ad a d e) or (ad a b c)), from M₂ h₃,
+                    show ad a d e, from (T₂ h₈ h₇) h₁ h₂
+
+        
             end ad
         end wr
     end hilbert
