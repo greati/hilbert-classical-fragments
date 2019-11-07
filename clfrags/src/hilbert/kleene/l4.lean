c713a8c01ee1708b41c697f94ac91900acd06841
Vitor Greati <greati@ufrn.edu.br>
2019-11-07 00:49:50 -0300

    Update calculus for l4

--- clfrags/src/hilbert/kleene/l4.lean
+++ clfrags/src/hilbert/kleene/l4.lean
@@ -5,6 +5,8 @@ constant and : Prop → Prop → Prop
 constant or : Prop → Prop → Prop
 constant imp : Prop → Prop → Prop
 constant neg : Prop → Prop
+constant top : Prop
+constant bot : Prop
 
 notation a `or` b := or a b
 notation a `imp` b := imp a b
@@ -21,6 +23,8 @@ axiom cl₈ : ∀ {a b : Prop}, b imp (a or b)
 axiom cl₉ : ∀ {a b c : Prop}, (a imp c) imp ((b imp c) imp ((a or b) imp c))
 axiom cl₁₀ : ∀ {a b : Prop}, (a imp b) imp ((a imp (neg b)) imp (neg a))
 axiom cl₁₁ : ∀ {a : Prop}, (neg (neg a)) imp a
+axiom cl₁₂ : top
+axiom cl₁₃ : ∀ {a : Prop}, bot imp a
 
 example {a : Prop} : a imp a := 
     have h₁ : (a imp ((a imp a) imp a)) imp ((a imp (a imp a)) imp (a imp a)), from cl₃,
@@ -29,7 +33,7 @@ example {a : Prop} : a imp a :=
     have h₄ : a imp (a imp a), from cl₂,
     show a imp a, from cl₁ h₄ h₃
 
-theorem cl₁₂ {a b c : Prop} (h₁ : a imp b) (h₂ : b imp c) : a imp c := 
+theorem cl₁₄ {a b c : Prop} (h₁ : a imp b) (h₂ : b imp c) : a imp c := 
     have h₃ : (a imp (b imp c)) imp ((a imp b) imp (a imp c)), from cl₃,
     have h₄ : (b imp c) imp (a imp (b imp c)), from cl₂,
     have h₅ : a imp (b imp c), from cl₁ h₂ h₄,
