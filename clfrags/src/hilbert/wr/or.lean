18a21ac6b2e890060eb4ae65752fc0245394d226
Vitor Greati <greati@ufrn.edu.br>
2019-11-15 09:58:30 -0300

    Put forall in or spec

--- clfrags/src/hilbert/wr/or.lean
+++ clfrags/src/hilbert/wr/or.lean
@@ -4,10 +4,10 @@ namespace clfrags
     namespace hilbert
         namespace wr
             namespace or
-                axiom d₁ : Π {a b : Prop}, a → or a b
-                axiom d₂ : Π {a : Prop}, or a a → a
-                axiom d₃ : Π {a b : Prop}, or a b → or b a
-                axiom d₄ : Π {a b c : Prop}, or a (or b c) → or (or a b) c
+                axiom d₁ : ∀ {a b : Prop}, a → or a b
+                axiom d₂ : ∀ {a : Prop}, or a a → a
+                axiom d₃ : ∀ {a b : Prop}, or a b → or b a
+                axiom d₄ : ∀ {a b c : Prop}, or a (or b c) → or (or a b) c
             end or 
         end wr
     end hilbert
